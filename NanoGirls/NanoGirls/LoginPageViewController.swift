0672e7906b14c22d88d5ed1d5070a7f7464c0e61
Cara Pennington <carap@email.sc.edu>
2016-10-19 00:06:23 -0400

    LoginPage Class

---
+++ NanoGirls/NanoGirls/LoginPageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginPageViewController.swift
+//  NanoGirls
+//
+//  Created by Cara Pennington on 10/18/16.
+//  Copyright © 2016 NanoBadgers. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
