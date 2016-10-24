cb774ac0697d559c1c9e56232f18b1f4d2b21097
bprue <pruette95@gmail.com>
2016-10-24 14:40:11 -0400

    Removed deprecated navigation, organized folders

---
+++ NanoGirls/NanoGirls/QuestionPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  QuestionPageViewController.swift
+//  NanoGirls
+//
+//  Created by John Chandler on 10/23/16.
+//  Copyright © 2016 NanoBadgers. All rights reserved.
+//
+
+import UIKit
+
+
+class QuestionPageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
