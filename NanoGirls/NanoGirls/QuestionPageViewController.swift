f6446f5ae79d232d8cfb4d4b6dabd0b41701c8ec
bprue <pruette95@gmail.com>
2016-12-05 22:44:18 -0500

    Fixed more constraints, added sample question and user interaction

--- NanoGirls/NanoGirls/QuestionPageViewController.swift
+++ NanoGirls/NanoGirls/QuestionPageViewController.swift
@@ -13,8 +13,40 @@ import UIKit
 
 class QuestionPageViewController: UIViewController {
     
+        let colors = Colors()
+    
+    @IBAction func correctAnswer(_ sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Level 1", message:
+            "Correct!", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func incorrect1(_ sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Level 1", message:
+            "Sorry, incorrect!", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Try again", style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func incorrect2(_ sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Level 1", message:
+            "Sorry, incorrect!", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Try again", style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refresh()
         
         // Do any additional setup after loading the view.
     }
@@ -24,15 +56,12 @@ class QuestionPageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    func refresh()
+    {
+        //sets the background gradient
+        view.backgroundColor = UIColor.clear
+        let backgroundLayer = colors.gl
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, at: 0)
+    }
 }
